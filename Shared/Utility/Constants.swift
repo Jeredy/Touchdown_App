02c055a35682bb77dfdb468c581440653ebd8c05
Andre Rodrigues <andrecomerciale@gmail.com>
2022-07-23 12:09:19 -0300

    feat: create FooterView and NavigationBarView.

---
+++ Shared/Utility/Constants.swift
@@ -0,0 +1,20 @@
+//
+//  Constants.swift
+//  Touchdown
+//
+//  Created by André Almeida on 2022-07-23.
+//
+
+import SwiftUI
+
+// DATA
+// COLOR
+let colorBackground: Color = Color("ColorBackground")
+let colorGray: Color = Color(UIColor.systemGray4)
+// LAYOUT
+// UX
+// API
+// IMAGE
+// FONT
+// STRING
+// MISC
---
+++ Shared/View/Home/LogoView.swift
@@ -0,0 +1,37 @@
+//
+//  LogoView.swift
+//  Touchdown
+//
+//  Created by André Almeida on 2022-07-23.
+//
+
+import SwiftUI
+
+struct LogoView: View {
+    var body: some View {
+        HStack {
+            Text("Touch".uppercased())
+                .font(.title3)
+                .foregroundColor(.black)
+                .fontWeight(.black)
+            
+            Image("logo-dark")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 30, height: 30, alignment: .center)
+            
+            Text("Down".uppercased())
+                .font(.title3)
+                .foregroundColor(.black)
+                .fontWeight(.black)
+        }
+    }
+}
+
+struct LogoView_Previews: PreviewProvider {
+    static var previews: some View {
+        LogoView()
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
