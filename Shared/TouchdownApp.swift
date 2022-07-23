ed25088e06543529b69f1c1b90e51d428b5cfd5e
Andre Rodrigues <andrecomerciale@gmail.com>
2022-07-23 11:01:03 -0300

    Initial Commit

---
+++ Shared/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Shared
+//
+//  Created by André Almeida on 2022-07-23.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
