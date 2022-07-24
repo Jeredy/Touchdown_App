8e5c4463da22cedf061c6a814d15b3a88fd80b3f
Andre Rodrigues <andrecomerciale@gmail.com>
2022-07-24 17:38:04 -0300

    feat: created the FeaturedView, CategoryView and ProductView

--- Shared/App/ContentView.swift
+++ Shared/App/ContentView.swift
@@ -20,9 +20,28 @@ struct ContentView: View {
                     .padding(.top, UIApplication.shared.windows.first?.safeAreaInsets.top)
                     .background(Color.white)
                     .shadow(color: Color.black.opacity(0.05), radius: 5, x: 0,y: 5)
-                Spacer()
-                FooterView()
-                    .padding(.horizontal)
+                ScrollView(.vertical, showsIndicators: false, content: {
+                    VStack(spacing: 0) {
+                        FeaturedTabView()
+                            .padding(.vertical, 20)
+                            .frame(height: UIScreen.main.bounds.width / 1.475)
+                        
+                        CategoryGridView()
+                        
+                        TitleView(title: "Helmet")
+                        
+                        LazyVGrid(columns: gridLayout, spacing: 15, content: {
+                            ForEach(products) { product in
+                                ProductItemView(product: product)
+                            }//: LOOP
+                        })//: GRID
+                        .padding(15)
+                        
+                        FooterView()
+                            .padding(.horizontal)
+                    }//: VSTACK
+                })//: SCROLL
+               
             }//:VSTACK
             .background(colorBackground.ignoresSafeArea(.all, edges: .all))
         }//:ZSTACK
